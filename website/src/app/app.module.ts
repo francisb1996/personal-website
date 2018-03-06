ccc28881218e7f5bdfbed49aeeadd973908982d1
CORPORATE\bjf0323 <brad.francis@daugherty.com>
2018-03-06 13:21:18 -0600

    added sidenav. working on responsive

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -2,9 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+
+// Material
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// For support of Material animations in certain browsers
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -20,9 +29,18 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
+  ]
 })
 export class AppModule { }
